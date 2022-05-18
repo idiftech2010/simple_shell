ede26df5ae8ea02025aa58a4ef280dd82326076f
idiftech2010 <idiftech2010@yahoo.com>
2022-05-18 02:16:11 -0700

    Simple Shell

--- shell.c
+++ shell.c
@@ -1,32 +1,50 @@
 #include "shell.h"
 /**
- * main - Simple Shell
- * @ac: Argument counter.
- * @av: Argument values.
- * @env: Environment variables.
- *
- * Return: 0 or -1 in failure.
- */
-int main(int ac, char **av, char **env)
-{
-	/* char *shell_pharse; */
-	command_t **cmd_list = NULL;/* Command List */
-	char *path = NULL;
-	/* history_t **history = malloc(sizeof(history_t)); */
+* main - carries out the read, execute then print output loop
+* @ac: argument count
+* @av: argument vector
+* @envp: environment vector
+*
+* Return: 0
+*/
 
-	path = find_path(env);
-	ac++;
-	/* shell_pharse = isatty(STDIN_FILENO) ? "> " : NULL; */
+int main(int ac, char **av, char *envp[])
+{
+	char *line = NULL, *pathcommand = NULL, *path = NULL;
+	size_t bufsize = 0;
+	ssize_t linesize = 0;
+	char **command = NULL, **paths = NULL;
+	(void)envp, (void)av;
+	if (ac < 1)
+		return (-1);
+	signal(SIGINT, handle_signal);
 	while (1)
 	{
-		cmd_list = _prompt(av[0], av[1]); /* get commands from cmd_line */
-		if (cmd_list)
-		{
-			if (_fork(av[0], *cmd_list, path, env))
-				error_handler(av[0], 102);
-		}
+		free_buffers(command);
+		free_buffers(paths);
+		free(pathcommand);
+		prompt_user();
+		linesize = getline(&line, &bufsize, stdin);
+		if (linesize < 0)
+			break;
+		info.ln_count++;
+		if (line[linesize - 1] == '\n')
+			line[linesize - 1] = '\0';
+		command = tokenizer(line);
+		if (command == NULL || *command == NULL || **command == '\0')
+			continue;
+		if (checker(command, line))
+			continue;
+		path = find_path();
+		paths = tokenizer(path);
+		pathcommand = test_path(paths, command[0]);
+		if (!pathcommand)
+			perror(av[0]);
 		else
-			error_handler(av[0], 103);
+			execution(pathcommand, command);
 	}
+	if (linesize < 0 && flags.interactive)
+		write(STDERR_FILENO, "\n", 1);
+	free(line);
 	return (0);
 }
